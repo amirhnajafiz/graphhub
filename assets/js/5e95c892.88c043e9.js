16001fa32ecee98529445a29eaa04c14303a767e
amirhnajafiz <najafizadeh21@gmail.com>
2024-09-29 20:35:44 -0400

    add: build app

---
+++ assets/js/5e95c892.88c043e9.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[9647],{7121:(e,s,r)=>{r.r(s),r.d(s,{default:()=>l});r(6540);var a=r(4164),p=r(1213),u=r(7559),c=r(2831),n=r(781),t=r(4848);function l(e){return(0,t.jsx)(p.e3,{className:(0,a.A)(u.G.wrapper.docsPages),children:(0,t.jsx)(n.A,{children:(0,c.v)(e.route.routes)})})}}}]);
