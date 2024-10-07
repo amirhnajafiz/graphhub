5309be8aca8b62f410d3a7f5ef9fefa672432bf5
amirhnajafiz <najafizadeh21@gmail.com>
2024-10-06 20:01:14 -0400

    fix: release bug

--- release.sh
+++ release.sh
@@ -3,17 +3,19 @@
 # remove old app files
 echo "Removing old app files..."
 rm -rf assets blog docs img markdown-page
-rm -f .nojeykll 404.html index.html sitemap.xml
+rm -f .nojekyll 404.html index.html sitemap.xml
+
+# build the new app
+cd app
+npm run build
+cd ..
 
 # check if the app build directory exists
 if [ -d "app/build" ]; then
     echo "Moving new app build files..."
     mv app/build/* ./
-    # Handle hidden files (e.g., .htaccess)
-    shopt -s dotglob
-    mv app/build/.* ./
-    shopt -u dotglob
     echo "New app files have been moved successfully."
+    rmdir app/build
 else
     echo "Error: app/build directory does not exist."
     exit 1
